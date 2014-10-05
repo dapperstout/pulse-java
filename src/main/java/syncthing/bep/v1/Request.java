3c322260de23addc7b5f7887a093e5065e7fec00
dapperstout <sir@dapperstout.com>
2014-10-05 20:17:30 +0200

    Cleanup.

--- src/main/java/syncthing/bep/v1/Request.java
+++ src/main/java/syncthing/bep/v1/Request.java
@@ -5,6 +5,6 @@ import static syncthing.bep.util.Xdr.xdr;
 public class Request extends Message {
 
     public Request(String repository, String name, long offset, int size) {
-        super((byte) 2, xdr(repository, name, offset, size), true);
+        super((byte) 2, xdr(repository, name, offset, size));
     }
 }
